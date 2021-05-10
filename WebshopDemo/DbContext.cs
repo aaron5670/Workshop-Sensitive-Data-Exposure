7d2f6e05c1c102eca4c07574fafc1463967acbba
Aaron van den Berg <a.vdberg98@gmail.com>
2021-05-10 16:34:37 +0200

    ✨ Initial commit

---
+++ WebshopDemo/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebshopDemo
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=CustomerDB.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Entity<Invoice>().ToTable("Invoices");
+            modelBuilder.Entity<InvoiceItem>().ToTable("InvoiceItems");
+        }
+    }
+}
---
+++ WebshopDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WebshopDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var context = new MyContext();
+            var customer = new Customer
+            {
+                CustomerId = 1,
+                FirstName = "Elizabeth",
+                LastName = "Lincoln",
+                Address = "23 Tsawassen Blvd."
+            };
+
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+    }
+}
